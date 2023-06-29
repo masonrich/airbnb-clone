100eb3669cf11821e061deee52ebb3cc4b3a70de
masonrich <masonjamesrich@gmail.com>
2023-06-28 20:24:20 -0600

    add categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from './Search';
 import UserMenu from './UserMenu';
 import { SafeUser } from '@/app/types';
+import Categories from './Categories';
 
 interface NavbarProps {
     currentUser?: SafeUser | null
@@ -34,6 +35,7 @@ export const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     )
 }
