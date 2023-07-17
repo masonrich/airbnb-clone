fdc99a342541cb44f3703e6ec9b2aa98be4850bc
masonrich <masonjamesrich@gmail.com>
2023-07-16 20:19:55 -0600

    add force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { getCurrentUser } from "./actions/getCurrentUser";
 import getListings, { IListingParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
@@ -5,7 +6,6 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 import { SafeListing } from "./types";
-export const dynamic = 'auto';
 interface HomeProps {
   searchParams: IListingParams
 }
