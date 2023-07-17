acfacd77623be31149da6cba9ed692fb61cf2e9e
masonrich <masonjamesrich@gmail.com>
2023-07-16 20:09:36 -0600

    add dynamic export

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 import { SafeListing } from "./types";
-
+export const dynamic = 'auto';
 interface HomeProps {
   searchParams: IListingParams
 }
